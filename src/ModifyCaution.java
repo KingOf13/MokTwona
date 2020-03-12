bb88c0d0d38b5c476dd890487f4dc97474a858b8
Julien Aigret <julien.aigret@student.uclouvain.be>
2020-03-12 16:49:46 +0100

    Front end caution

--- src/ModifyCaution.java
+++ src/ModifyCaution.java
@@ -9,10 +9,13 @@ public class ModifyCaution {
     private JComboBox selectBox;
     private JButton fiveButton;
     private JButton tenButton;
+    private JButton confirmerButton;
     private JFrame frame;
     private final String[] exName = {"Pierre", "Paul", "Jean", "Jacques"};
     private final String[] addOrRemove = {"Ajouter", "Retirer"};
     private int[] exCaution = {10, 5 , 0, 5};
+    private int montant = 0;
+    private boolean notSet = true;
 
     public ModifyCaution(JFrame frame) {
         this.frame = frame;
@@ -27,6 +30,49 @@ public class ModifyCaution {
             public void actionPerformed(ActionEvent e) {
                 int idx = clientBox.getSelectedIndex();
                 cautionPanel.setText(exName[idx] + " a une caution de " + exCaution[idx] + "€");
+            }
+        });
+        fiveButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                montant = 5;
+                if (clientBox.getSelectedIndex() != -1 && selectBox.getSelectedIndex() != -1) {
+                    cautionPanel.setText(addOrRemove[selectBox.getSelectedIndex()] + " " + montant + "€ à " + exName[clientBox.getSelectedIndex()] + " ?");
+                    notSet = false;
+                }
+                else {
+                    cautionPanel.setText("Choisir un client et une action PUIS le montant");
+                }
+
+            }
+        });
+        tenButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                montant = 10;
+                if (clientBox.getSelectedIndex() != -1 && selectBox.getSelectedIndex() != -1) {
+                    cautionPanel.setText(addOrRemove[selectBox.getSelectedIndex()] + " " + montant + "€ à " + exName[clientBox.getSelectedIndex()] + " ?");
+                    notSet = false;
+                }
+                else {
+                    cautionPanel.setText("Choisir un client et une action PUIS le montant");
+                }
+
+            }
+        });
+        confirmerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (clientBox.getSelectedIndex() == -1 || selectBox.getSelectedIndex() == -1 || montant == 0 || notSet) {
+                    String toPrint = "";
+                    if (clientBox.getSelectedIndex() == -1) toPrint += "Sélectionner un client dans la base de donnée\n";
+                    if (selectBox.getSelectedIndex() == -1) toPrint += "Sélectionner une action\n";
+                    if (montant == 0) toPrint += "Sélectionner un montant";
+                }
+                else {
+                    System.out.println(addOrRemove[selectBox.getSelectedIndex()] + " " + montant + "€ à " + exName[clientBox.getSelectedIndex()]);
+                    frame.dispose();
+                }
             }
         });
     }
